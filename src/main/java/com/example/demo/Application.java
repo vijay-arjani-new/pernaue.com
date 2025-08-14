b463533f61b1bc0c220ebbbaa8d6c2497ec58a0a
vijay-arjani-new <vijayarjani03@gmail.com>
2025-08-15 03:34:51 +0530

    Checking the application

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("Hi Pernaue.com");
+		System.out.println("Hi Vijay Arjani"); 
 	}
 
 }
